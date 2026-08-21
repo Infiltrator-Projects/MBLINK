b8c17a91daa450bde770bc6c9bb84af73af3181e
Shannon Smith <infiltratr@yandex.com>
2026-08-21 20:24:11 +1000

    Use external OM651 catalogue ABI on iPhone

--- app/ios/MBLINK/MBLINK-Bridging-Header.h
+++ app/ios/MBLINK/MBLINK-Bridging-Header.h
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 #import "MBLinkDiagnosticsController.h"
 #include "mblink/parameter.h"
-#include "mblink/mercedes_om651.h"
+#include "mblink/mercedes_om651_api.h"
