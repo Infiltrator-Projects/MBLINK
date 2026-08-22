cf03124d3ec26aae84665390864a4299f1ddad61
Shannon Smith <infiltratr@yandex.com>
2026-08-22 15:52:48 +1000

    Add MBLINK forwarding header for LINK workspace

---
+++ include/link/workspace.h
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+#ifndef MBLINK_LINK_WORKSPACE_FORWARD_H
+#define MBLINK_LINK_WORKSPACE_FORWARD_H
+
+#include "../../src/link/include/link/workspace.h"
+
+#endif
