99c32703e954ab191cad931258ad22a546d67904
Shannon Smith <infiltratr@yandex.com>
2026-08-22 16:58:40 +1000

    Forward shared LINK parameter contract

---
+++ include/link/parameter.h
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+#ifndef MBLINK_LINK_PARAMETER_FORWARD_H
+#define MBLINK_LINK_PARAMETER_FORWARD_H
+#include "../../src/link/include/link/parameter.h"
+#endif
