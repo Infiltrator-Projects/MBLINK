a919710c0642f5ed5aff569c65d5b2f38488969f
Shannon Smith <infiltratr@yandex.com>
2026-08-23 19:26:14 +1000

    Compile LINK DTC knowledge in the iOS OBD bridge

--- src/obd2/obd2.c
+++ src/obd2/obd2.c
@@ -13,6 +13,7 @@
 
 #if defined(__APPLE__) && TARGET_OS_IOS
 #include "../link/src/obd2/obd2.c"
+#include "../link/src/obd2/dtc_knowledge.c"
 #else
 typedef int mblink_obd2_compat_translation_unit;
 #endif
