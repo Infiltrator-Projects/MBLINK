c04b7f0d7ddc5b25c601536c7350896cc8dfda6d
Shannon Smith <infiltratr@yandex.com>
2026-08-23 19:27:12 +1000

    Expose diagnostic fault knowledge to iPhone UI

--- app/ios/MBLINK/MBLINK-Bridging-Header.h
+++ app/ios/MBLINK/MBLINK-Bridging-Header.h
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 #import "MBLinkDiagnosticsController.h"
+#include "mblink/obd2.h"
 #include "mblink/parameter.h"
 #include "mblink/mercedes_om651_api.h"
