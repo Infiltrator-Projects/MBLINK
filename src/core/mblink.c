dbd1e6d39b21f29fd68bafed87c7a32d0a12042f
Shannon Smith <infiltratr@yandex.com>
2026-09-01 15:00:58 +1000

    Adopt LINK 0.14.59 OBD standards engine

--- src/core/mblink.c
+++ src/core/mblink.c
@@ -30,7 +30,7 @@
  * external immutable LINK commit and is checked against the gitlink in CI.
  */
 #define MBLINK_EMBEDDED_LINK_REVISION \
-    "539f6416c23901608bab2c0ddb6db47be2b98c31"
+    "78764191e29f299db18d74a2374f3d0c442b4a77"
 
 /*
  * Normal CMake builds consume shared engines through LINK::Core. The native
@@ -42,6 +42,7 @@
 #include "../link/src/core/workspace.c"
 #include "../link/src/core/fuel_economy.c"
 #include "../link/src/core/diagnostic_request.c"
+#include "../link/src/core/doip.c"
 #include "../link/src/core/diagnostic_flow.c"
 #include "../link/src/core/parameter.c"
 #include "../link/src/core/scheduler.c"
