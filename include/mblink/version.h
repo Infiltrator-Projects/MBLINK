5531e1fee78b422930c556acb376918f08c1d9c5
Shannon Smith <infiltratr@yandex.com>
2026-09-06 08:12:25 +1000

    Release 0.7.179: publish latest MBLINK

--- include/mblink/version.h
+++ include/mblink/version.h
@@ -1,6 +1,11 @@
-#ifndef MBLINK_VERSION_H
-#define MBLINK_VERSION_H
+#pragma once
 
-#define MBLINK_VERSION_STRING "0.7.178"
+#define MBLINK_VERSION_MAJOR 0
+#define MBLINK_VERSION_MINOR 7
+#define MBLINK_VERSION_PATCH 179
 
-#endif
+#define MBLINK_VERSION_STRING "0.7.179"
+
+namespace mblink {
+inline constexpr const char* kVersion = MBLINK_VERSION_STRING;
+}  // namespace mblink
