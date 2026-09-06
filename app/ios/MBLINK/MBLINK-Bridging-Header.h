e43e65ec225d3401c7468619cc29d244af488436
Shannon Smith <infiltratr@yandex.com>
2026-09-06 20:09:03 +1000

    Repair Apple source ownership after LINK split

--- app/ios/MBLINK/MBLINK-Bridging-Header.h
+++ app/ios/MBLINK/MBLINK-Bridging-Header.h
@@ -8,5 +8,5 @@
 #include "mblink/mercedes_vin.h"
 #include "mblink/mercedes_om651_api.h"
 #include "link/i18n.h"
-#include "link/mercedes_me_data_ids.h"
+#include "mblink/mercedes_me_data_ids.h"
 #include "link/dashboard.h"
