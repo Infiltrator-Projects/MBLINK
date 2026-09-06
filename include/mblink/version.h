92ff56c436e2faa43b67db6832cd9d9619d8f578
Shannon Smith <infiltratr@yandex.com>
2026-09-06 14:37:02 +1000

    Document MBLINK version synchronization invariant

--- include/mblink/version.h
+++ include/mblink/version.h
@@ -1,6 +1,7 @@
 #ifndef MBLINK_VERSION_H
 #define MBLINK_VERSION_H
 
+/* Keep this source identity synchronized with VERSION and platform metadata. */
 #define MBLINK_VERSION_STRING "0.7.181"
 
 #endif
