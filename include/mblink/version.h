2d781890eb50560adc3b24e79c055fad89feb845
Shannon Smith <infiltratr@yandex.com>
2026-09-14 16:02:32 +1000

    Release 0.7.201: publish Mercedes Dashboard icon fix

--- include/mblink/version.h
+++ include/mblink/version.h
@@ -2,6 +2,6 @@
 #define MBLINK_VERSION_H
 
 /* Keep this source identity synchronized with VERSION and platform metadata. */
-#define MBLINK_VERSION_STRING "0.7.200"
+#define MBLINK_VERSION_STRING "0.7.201"
 
 #endif
