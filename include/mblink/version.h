93278caad6664f04f901c760d9e1597cb4647a22
Shannon Smith <infiltratr@yandex.com>
2026-09-18 19:34:45 +1000

    Release 0.7.204: adopt LINK 0.15.24 and Common 1.19.2

--- include/mblink/version.h
+++ include/mblink/version.h
@@ -2,6 +2,6 @@
 #define MBLINK_VERSION_H
 
 /* Keep this source identity synchronized with VERSION and platform metadata. */
-#define MBLINK_VERSION_STRING "0.7.203"
+#define MBLINK_VERSION_STRING "0.7.204"
 
 #endif
